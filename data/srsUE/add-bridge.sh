b17dde39f1f9ee10e83b76f10bad82cbf37d4cf4
orikaito <r03a05@ed.cc.suzuka-ct.ac.jp>
2022-07-21 17:10:18 +0900

    :wrench: Add UE's default route
    UEのCareer側のルートをデフォルトに変更

--- data/srsUE/add-bridge.sh
+++ data/srsUE/add-bridge.sh
@@ -6,7 +6,7 @@ ip link set UE-C_to_app netns ue-career up
 ip addr add 10.10.2.2/24 dev app_to_UE-C
 ip link set app_to_UE-C up
 ip netns exec ue-career ip addr add 10.10.2.1/24 dev UE-C_to_app
-ip route add 192.168.30.0/24 via 10.10.2.1
+ip route add default via 10.10.2.1
 # edge
 ip netns add ue-edge
 ip link add UE-E_to_app type veth peer name app_to_UE-E
