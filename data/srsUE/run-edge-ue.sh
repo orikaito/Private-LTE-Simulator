0085f2f9dbd760b21c23ae94ea837e73549ad366
orikaito <r03a05@ed.cc.suzuka-ct.ac.jp>
2022-12-22 14:58:39 +0900

    :wrench: Modify enb or ue conf files
    構成変更に伴うeNBとUEのコンフィグファイルを修正

--- data/srsUE/run-edge-ue.sh
+++ data/srsUE/run-edge-ue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SRSEPC_PATH=/Documents/srsRAN_edge
+SRSEPC_PATH=/Documents/srsRAN_edge/build
 DATA_PATH=/Documents/Private-LTE-Simulator/data/srsUE
 "${HOME}${SRSEPC_PATH}/srsue/src/srsue" "${HOME}${DATA_PATH}/ue-edge.conf"
